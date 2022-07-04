57c973827783a00ff5e0bd81b8fc75b62aebe1b2
christylernz <7918463+christylernz@users.noreply.github.com>
2022-07-04 22:05:02 +1200

    WIP load Google Auth js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+import googleAuth from "./google-auth";
+
 const client_id = '702269871560-c4lmbm1t9cdlq4ce7otvjjltsrt3lg1c.apps.googleusercontent.com';
 
+
+
 function main() {
+    googleAuth.load(client_id);
     const signout = document.getElementById('signout');
     signout.style.display = 'none';
     const signout_btn = document.getElementById('signout_button');
