7c000223a3bd179d0aa25488dee09feb299956f2
christylernz <7918463+christylernz@users.noreply.github.com>
2022-07-07 15:19:48 +1200

    fix signin fuction case issue

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function main() {
     signout.style.display = 'none';
     const signout_btn = document.getElementById('signout_button');
     const revoke_btn = document.getElementById('revoke_button');
-    googleAuth.showSignin('signin', {type: 'standard', size: 'large', text: 'signup_with'});
+    googleAuth.showSignIn('signin', {type: 'standard', size: 'large', text: 'signup_with'});
 }
 
 main();
